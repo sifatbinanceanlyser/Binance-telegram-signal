2a045663801587b8384edfb013bcf3b48f4f6229
sifatbinanceanlyser <tonmoyahmmed60@gmail.com>
2026-09-12 01:40:37 +0300

    Update stable_api.py

--- quotexapi/stable_api.py
+++ quotexapi/stable_api.py
@@ -1,5 +1,3 @@
-
-# python
 from quotexapi.api import QuotexAPI
 import quotexapi.global_value as global_value
 import threading
@@ -29,11 +27,9 @@ class Quotex:
         # --start
         # self.connect()
         # this auto function delay too long
-        
-        
 
     def change_account(self, Balance_MODE):
-        """Change active account `real` or `practice`"""
+        """Change active account 'real' or 'practice'"""
         real_id = None
         practice_id = None
         if Balance_MODE == "REAL":
@@ -46,7 +42,6 @@ class Quotex:
             
     def get_balance(self):
         pass
-        
 
     def get_balances(self):
         pass
@@ -55,27 +50,24 @@ class Quotex:
 
     # __________________FOR OPTION____________________________
     def buy(self, ACTIVES, price, ACTION, expirations):
-        """ Buy Binary option""""
+        """Buy Binary option"""
         pass
       
     def sell_option(self, options_ids):
         pass
       
     def check_win(self, id_number):
-        """Check win based id""""
+        """Check win based id"""
         pass
       
     def get_signal_data(self):
-      """ Get signal Quotex server""""
+        """Get signal Quotex server"""
         pass
       
-    def get_payment(self)
-        """ payment Quotex server""""
+    def get_payment(self):
+        """payment Quotex server"""
         pass
 
-      
-      
-      
     # ________________________________________________________________________
     # _______________________        CANDLE      _____________________________
     # ________________________self.api.getcandles() wss________________________
@@ -94,29 +86,27 @@ class Quotex:
             except:
                 logging.error('**error** get_candle_v2 need reconnect')
                 self.connect() #go connect
+
     # ------------------------Subscribe ONE SIZE-----------------------
     def start_candles_one_stream(self, ACTIVE, size):
         pass
+
     def stop_candles_one_stream(self, ACTIVE, size):
         pass
-    
-      
-    # ------------------------Subscribe ALL SIZE-----------------------
 
+    # ------------------------Subscribe ALL SIZE-----------------------
     def start_candles_all_size_stream(self, ACTIVE):
         pass
+
     def stop_candles_all_size_stream(self, ACTIVE):
         pass
-      
-      
-      
+
     #######################################################
     # ______________________________________________________
     # _____________________REAL TIME CANDLE_________________
     # ______________________________________________________
     #######################################################
     def start_candles_stream(self, ACTIVE, size, maxdict):
-
         if size == "all":
             for s in self.size:
                 pass
@@ -125,7 +115,7 @@ class Quotex:
         else:
             logging.error('**error** start_candles_stream please input right size')
             
-     def stop_candles_stream(self, ACTIVE, size):
+    def stop_candles_stream(self, ACTIVE, size):
         if size == "all":
             pass
         elif size in self.size:
@@ -148,16 +138,13 @@ class Quotex:
                 return False
         else:
             logging.error('**error** get_realtime_candles() please input right "size"')
-            
-            
+
     def re_subscribe_stream(self):
         try:
             pass
         except:
             pass
-        
-      
-      
+
     def connect(self):
         try:
             self.api.close()
@@ -172,13 +159,13 @@ class Quotex:
             return True, None
         else:
             return False, reason
-          
+
     def close(self):
         try:
             self.api.close()
         except:
             pass
-          
+
     def check_connect(self):
         # True/False
         # if not connected, sometimes it's None, sometimes its '0', so
@@ -187,3 +174,4 @@ class Quotex:
             return False
         else:
             return True
+            
